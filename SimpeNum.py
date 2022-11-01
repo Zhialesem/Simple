21a2bda862a37d8e0dd162dbb702cecdaa522dcb
AZI <zhialesem@gmail.com>
2022-11-01 15:15:36 +0300

    Убрал 2 из списка

--- SimpeNum.py
+++ SimpeNum.py
@@ -3,16 +3,23 @@
 import time
 start = time.time() ## точка отсчета времени
 
+from math import sqrt
+
 n = int(input("n="))
 lst=[]
-for i in range(2, n):
-    # пробегаем по списку (lst) простых чисел
+for i in range(1, n+1, 2):
+    if (i > 10) and (i%10==5):
+        continue
     for j in lst:
-        if i % j == 0:
+        if j > int((sqrt(i)) + 1):
+            lst.append(i)
+            break
+        if (i % j == 0):
             break
     else:
         lst.append(i)
 print (lst)
 end = time.time() - start ## собственно время работы программы
 
-print(end) ## вывод времени
+
+
