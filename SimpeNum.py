d7534960b5e0204f511a6fc08490523766a75be4
AZI <zhialesem@gmail.com>
2022-11-07 15:25:47 +0300

    Убрал 2 из списка

--- SimpeNum.py
+++ SimpeNum.py
@@ -7,18 +7,19 @@ from math import sqrt
 
 n = int(input("n="))
 lst=[]
-for i in range(1, n+1, 2):
-    if (i > 10) and (i%10==5):
-        continue
-    for j in lst:
-        if j > int((sqrt(i)) + 1):
-            lst.append(i)
+for i in range(2, n):
+    if (i > 10) :
+        if (i % 2 == 0) or (i % 10 == 5) :
+            continue
+    for j in lst :
+        if j > int ( (sqrt ( i ))+1 ) :
+            lst.append ( i )
             break
-        if (i % j == 0):
+        if (i % j == 0) :
             break
-    else:
-        lst.append(i)
-print (lst)
+    else :
+        lst.append ( i )
+print(lst)
 end = time.time() - start ## собственно время работы программы
 
 
